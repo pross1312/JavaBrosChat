8d7a93b13616543129d70dcfbcfa8a786464c3f7
21127335 <sphlam21@clc.fitus.edu.vn>
2023-12-01 14:32:42 +0700

    implement 2 functions get_login_log of AdminService

--- src/main/java/Server/Service/AdminService.java
+++ src/main/java/Server/Service/AdminService.java
@@ -12,8 +12,9 @@ import java.util.Optional;
 public class AdminService extends Service {
     ArrayList<UserInfo> list_users(String token) throws SQLException { // filter will be done on client side
         var acc = Server.accounts.get(token);
-        if (acc == null) throw new Error("Can't execute list_users api, token not found");
-        if(acc.b == AccountType.User)
+        if (acc == null)
+            throw new Error("Can't execute list_users api, token not found");
+        if (acc.b == AccountType.User)
             throw new Error("Only admin is allowed to get the user's list");
         ArrayList<UserInfo> user = UserInfoDb.list_users();
         user.trimToSize();
@@ -22,8 +23,9 @@ public class AdminService extends Service {
 
     ArrayList<UserInfo> list_active_users(String token, Date from, Date to) throws SQLException{ // sort by name or created time on client side
         var acc = Server.accounts.get(token);
-        if (acc == null) throw new Error("Can't execute list_users api, token not found");
-        if(acc.b == AccountType.User)
+        if (acc == null)
+            throw new Error("Can't execute list_active_users api, token not found");
+        if (acc.b == AccountType.User)
             throw new Error("Only admin is allowed to get the user's active list");
         ArrayList<UserInfo> user = LoginRecordDb.get_list_active_users(from, to);
         user.trimToSize();
@@ -31,8 +33,8 @@ public class AdminService extends Service {
     }
     void add_user(String token, String username, String pass, UserInfo user_info) throws SQLException{
         var acc = Server.accounts.get(token);
-        if(acc.a == null)
-            throw new Error("Empty token");
+        if (acc == null)
+            throw new Error("Can't execute add_user api, token not found");
         if(acc.b == AccountType.User)
             throw new Error("Only admin is allowed to insert a user");
         AccountService as = new AccountService();
@@ -40,8 +42,8 @@ public class AdminService extends Service {
     }
     void update_user(String token, String username, UserInfo user_info) throws SQLException{
         var acc = Server.accounts.get(token);
-        if(acc.a == null)
-            throw new Error("Empty token");
+        if (acc == null)
+            throw new Error("Can't execute update_user api, token not found");
         if(acc.b == AccountType.User)
             throw new Error("Only admin is allowed to update a user");
         if(UserInfoDb.query(username) == null)
@@ -52,8 +54,8 @@ public class AdminService extends Service {
     }
     void del_user(String token, String username) throws SQLException{
         var acc = Server.accounts.get(token);
-        if(acc.a == null)
-            throw new Error("Empty token");
+        if (acc == null)
+            throw new Error("Can't execute del_user api, token not found");
         if(acc.b == AccountType.User)
             throw new Error("Only admin is allowed to delete a user");
         if(UserInfoDb.query(username) == null)
@@ -63,25 +65,41 @@ public class AdminService extends Service {
     }
     void change_user_pass(String token, String username, String new_pass) throws SQLException{
         var acc = Server.accounts.get(token);
-        if(acc.a == null)
-            throw new Error("Empty token");
+        if (acc == null)
+            throw new Error("Can't execute change_user_pass api, token not found");
         if(acc.b == AccountType.User)
             throw new Error("Only admin is allowed to change a user's password");
         if(UserInfoDb.query(username) == null)
             throw new Error("Username does not exist to change password");
         AccountDb.change_pass(username, new_pass);
     }
-//    ArrayList<LoginRecordDb> get_login_log(String token, String username); // sort by time
-//    ArrayList<LoginRecordDb> get_login_log(String token); // all users, sort by time
-//    ArrayList<UserInfo> list_user_friends(String token, String username);
+    ArrayList<Date> get_login_log(String token, String username) throws SQLException{ // sort by time
+        var acc = Server.accounts.get(token);
+        if (acc == null)
+            throw new Error("Can't execute get_login_log api, token not found");
+        if(acc.b == AccountType.User)
+            throw new Error("Only admin is allowed to get a user's login record");
+        return LoginRecordDb.get_user_login_log(username);
+    }
+    ArrayList<LoginRecord> get_login_log(String token) throws SQLException{ // all users, sort by time
+        var acc = Server.accounts.get(token);
+        if (acc == null)
+            throw new Error("Can't execute get_login_log api, token not found");
+        if(acc.b == AccountType.User)
+            throw new Error("Only admin is allowed to get the login record of all users");
+        return LoginRecordDb.get_all_login_log();
+    }
+    ArrayList<UserInfo> list_user_friends(String token, String username){
+
+    }
 //    // group-operations
-//    //
-//    ArrayList<UserInfo> list_group_members(String token, String group_id); // filter out admins on client side (admin is also member)
+//    ArrayList<GroupChatInfo> list_groups(String token, String pattern); // sort and filter on client side
+//    ArrayList<GroupChatMemberInfo> list_group_members(String token, String group_id); // filter out admins on client side (admin is also member)
 //    // spam-operations
-//    ArrayList<SpamReport> list_spam_reports(String token); // all reports, sort by username or reported time on client side
-//    ArrayList<SpamReport> list_spam_reports(String token, Date from, Date to); // list report made between [from, to]
-//    ArrayList<SpamReport> list_spam_filter_name(String token, String pattern); // list report filter by username
-//    void lock_user(String token, String username); // confirm spam-report and lock user using this api
+//    ArrayList<SpamReport> list_spam_reports(String token) // all reports, sort by username or reported time on client side
+//    ArrayList<SpamReport> list_spam_reports(String token, Date from, Date to) // list report made between [from, to]
+//    ArrayList<SpamReport> list_spam_filter_name(String token, String pattern) // list report filter by username
+//    void lock_user(String token, String username) // confirm spam-report and lock user using this api
 //    // new-registration
-//    //
+//    ArrayList<RegistrationRecord> list_registers(String token, Date from, Date to) // list user-registrations made between [from, to] order by name or time on client side
 }
